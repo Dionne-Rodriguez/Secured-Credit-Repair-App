836313b22aaade27bfba56729c8833ed0b7168ee
Dionne <dionne.r.9@gmail.com>
2021-11-27 00:11:11 -0500

    Initial Commit

---
+++ Secured Credit Repair/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Secured Credit Repair
+//
+//  Created by Dionne Rodriguez on 11/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Secured Credit Repair/Secured_Credit_RepairApp.swift
@@ -0,0 +1,17 @@
+//
+//  Secured_Credit_RepairApp.swift
+//  Secured Credit Repair
+//
+//  Created by Dionne Rodriguez on 11/27/21.
+//
+
+import SwiftUI
+
+@main
+struct Secured_Credit_RepairApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
